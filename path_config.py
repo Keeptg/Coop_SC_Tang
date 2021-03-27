460d3ac3d812c517d0978e91b1a748d686378491
Keeptg <lifei15@outlook.com>
2021-03-27 11:38:09 +0800

    add WGJ-Group path to path_config.py

--- path_config.py
+++ path_config.py
@@ -1,16 +1,28 @@
 import os
+import sys
+import socket
 
-run_in_cluster = False
-script_dir = os.path.split(os.path.realpath(__file__))[0]
-root_dir = os.path.split(script_dir)[0]
-data_dir = os.path.join(root_dir, 'Data')
-if run_in_cluster:
+hostname = socket.gethostname()
+script_path = sys.path[0]
+
+
+if hostname in ['login01', 'login02'] or 'node' in hostname:
+    root_dir = '/home/users/lifei/Data/Pro_piao'
+    data_dir = os.path.join(root_dir, 'Data')
     working_dir = os.environ['WORKDIR']
-else:
-    working_dir = os.path.join(root_dir, 'working_dir')
-
-if run_in_cluster:
     cluster_dir = '/home/www/lifei/run_output/Pro_piao'
-else:
+    model_dir = os.path.join(root_dir, 'model_output')
+    for dir_ in [root_dir, data_dir, working_dir, cluster_dir, model_dir]:
+        if not os.path.exists(dir_):
+            os.makedirs(dir_)
+    run_in_cluster = True
+elif hostname is 'WGJ_Group':
+    root_dir = '/home/lifei/Data/code_project/Pro_piao'
+    data_dir = os.path.join(root_dir, 'Data')
+    working_dir = os.path.join(root_dir, 'working_dir')
     cluster_dir = os.path.join(root_dir, 'cluster_output')
-model_dir = os.path.join(root_dir, 'model_output')
+    model_dir = os.path.join(root_dir, 'model_output')
+    for dir_ in [root_dir, data_dir, working_dir, cluster_dir, model_dir]:
+        if not os.path.exists(dir_):
+            os.makedirs(dir_)
+    run_in_cluster = False
